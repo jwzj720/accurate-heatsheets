e9c201596c8eb85a59bc32f40428d165b3715961
Walt Jones - Server <jwzj720@gmail.com>
2024-01-30 16:43:47 +0000

    made class for DB queries

---
+++ API/db_query.py
@@ -0,0 +1,164 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+from typing import Dict, List
+import logging 
+
+load_dotenv('/home/wjones/CC/Capstone/tbd2/LACCTiC/.env', override=True)
+
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASS')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+
+class db_query:
+
+    def __init__(self):
+        self.db_params = {
+            'dbname': db_name,
+            'user': db_user,
+            'password' : db_password,
+            'host' : db_host,
+            'port' : db_port
+        }
+        self.conn = self.connect_db()
+
+          
+
+    def connect_db(self):
+        """Connect to the PostgreSQL database server."""
+        conn = psycopg2.connect(**self.db_params)
+        return conn
+    
+    def get_personal_best(self, first_name: str, last_name: str, distance: str) -> None:
+    
+        if distance == '8k':
+            one = '8k'
+            two = '8,000m'
+            three = '8000m'
+            four = '8.0k'
+            five = "8000"
+            six = "8"
+        elif distance == '10k':
+            one = '10k'
+            two = '10,000m'
+            three = '10000m'
+            four = '10.0k'
+            five = "10000"
+            six = "10"
+        elif distance == '5k':  
+            one = '5k'
+            two = '5,000m'
+            three = '5000m'
+            four = '5.0k'
+            five = "5000"
+            six = "5"
+        elif distance == '6k':
+            one = '6k'
+            two = '6,000m'
+            three = '6000m'
+            four = '6.0k'
+            five = "6000"
+            six = "6"
+        else:
+            print("Invalid distance")
+            return
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                SELECT 
+                    rn.firstname,
+                    rn.lastname,
+                    MIN(rr.time) AS personal_best_time
+                FROM 
+                    Runners rn
+                JOIN 
+                    RaceResults rr ON rn.lacctic_id = rr.lacctic_id
+                JOIN 
+                    Races r ON rr.meet_id = r.meet_id
+                WHERE 
+                    rn.firstname = %s AND rn.lastname = %s AND 
+                    (
+                        r.section LIKE %s OR
+                        r.section LIKE %s OR
+                        r.section LIKE %s OR
+                        r.section LIKE %s OR
+                        r.section LIKE %s OR
+                        r.section LIKE %s
+                    )
+                GROUP BY 
+                    rn.firstname, rn.lastname;
+            """, (first_name, last_name, "%"+one+"%", "%"+two+"%", "%"+three+"%", "%"+four+"%", "%"+five+"%", "%"+six+"%"))
+            
+            result = cur.fetchone()
+            if result:
+                return(result[2])
+            else:
+                return(None)
+
+    def get_season_best(self, first_name: str, last_name: str, year: int, distance: str) -> None:
+    
+        if distance == '8k':
+            one = '8k'
+            two = '8,000m'
+            three = '8000m'
+            four = '8.0k'
+            five = "8000"
+            six = "8"
+        elif distance == '10k':
+            one = '10k'
+            two = '10,000m'
+            three = '10000m'
+            four = '10.0k'
+            five = "10000"
+            six = "10"
+        elif distance == '5k':  
+            one = '5k'
+            two = '5,000m'
+            three = '5000m'
+            four = '5.0k'
+            five = "5000"
+            six = "5"
+        elif distance == '6k':
+            one = '6k'
+            two = '6,000m'
+            three = '6000m'
+            four = '6.0k'
+            five = "6000"
+            six = "6"
+        else:
+            print("Invalid distance")
+            return
+        with self.conn.cursor() as cur:
+            cur.execute("""
+                SELECT 
+                    rn.firstname,
+                    rn.lastname,
+                    MIN(rr.time) AS personal_best_time
+                FROM 
+                    Runners rn
+                JOIN 
+                    RaceResults rr ON rn.lacctic_id = rr.lacctic_id
+                JOIN 
+                    Races r ON rr.meet_id = r.meet_id
+                WHERE 
+                    rn.firstname = %s AND 
+                    rn.lastname = %s AND 
+                    EXTRACT(YEAR FROM r.date) = %s AND
+                    (
+                        r.section LIKE %s OR
+                        r.section LIKE %s OR
+                        r.section LIKE %s OR
+                        r.section LIKE %s OR
+                        r.section LIKE %s OR
+                        r.section LIKE %s
+                    )
+                GROUP BY 
+                    rn.firstname, rn.lastname;
+            """, (first_name, last_name, str(year), "%"+one+"%", "%"+two+"%", "%"+three+"%", "%"+four+"%", "%"+five+"%", "%"+six+"%"))
+            
+            result = cur.fetchone()
+            if result:
+                return(result[2])
+            else:
+                return(None)
