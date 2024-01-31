89fe4d15fe65ac71ec8e063eb43ec5a663939f3e
Walt Jones - Server <jwzj720@gmail.com>
2024-01-31 18:00:59 +0000

    added new API endpoint and Vue components

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,24 @@
 <template>
   <div id="app">
-    <h1>To-Do List</h1>
+    <h1>Upload</h1>
     <file-upload></file-upload>
-    <ul>
-      <li v-for="item in ToDoItems" :key="item.id">
-        <to-do-item
-          :label="item.label"
-          :done="item.done"
-          :id="item.id"></to-do-item>
-      </li>
-    </ul>
+    <h1>Download</h1>
+    <file-download></file-download>
   </div>
   </template>
 <script>
 import FileUpload from "./components/FileUpload.vue";
-import ToDoItem from "./components/ToDoItem.vue";
-import uniqueId from "lodash.uniqueid";
+import FileDownload from "./components/FileDownload.vue";
 
 export default {
   name: 'App',
   components: {
-    ToDoItem,
     FileUpload,
+    FileDownload,
   },
   data()
   {
     return {
-      ToDoItems: [
-        { id: uniqueId("todo-"), label: "Learn Vue", done: false },
-        {
-          id: uniqueId("todo-"),
-          label: "Create a Vue project with the CLI",
-          done: true,
-        },
-        { id: uniqueId("todo-"), label: "NOT", done: true },
-        { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
-      ],
     };
   },
 };
