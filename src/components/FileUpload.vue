e086b348a0dcd740947aa7c8237c9ec502a4e4de
Walt Jones - Server <jwzj720@gmail.com>
2024-02-05 16:56:50 +0000

    changed upload component

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,36 +1,4 @@
 <template>
-    <div>
-      <h2>Upload a PDF</h2>
-      <input type="file" @change="onFileChange" accept="application/pdf" />
-      <button @click="upload">Upload</button>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        selectedFile: null
-      }
-    },
-    methods: {
-      onFileChange(e) {
-        this.selectedFile = e.target.files[0];
-      },
-      upload() {
-        let formData = new FormData();
-        formData.append('file', this.selectedFile);
-  
-        axios.post('heatsheet-api.vercel.app/upload', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        }).then((response) => {
-          console.log('SUCCESS!');
-          // Get filename from the response
-          const filename = response.data.filename;
   <div>
     <h2>Upload a PDF</h2>
     <input type="file" @change="onFileChange" accept="application/pdf" />
