85011e122b414eb8d38915b9ad06d89f21381912
Walt Jones - Server <jwzj720@gmail.com>
2024-02-05 08:22:21 +0000

    changed CORS

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,42 +1,44 @@
 <template>
-    <div>
-      <h2>Upload a PDF</h2>
-      <input type="file" @change="onFileChange" accept="application/pdf" />
-      <button @click="upload">Upload</button>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        selectedFile: null
-      }
-    },
-    methods: {
-      onFileChange(e) {
-        this.selectedFile = e.target.files[0];
-      },
-      upload() {
-        let formData = new FormData();
-        formData.append('file', this.selectedFile);
-  
-        axios.post('api.valterbonez.com:8081/upload', formData, {
-            'Content-Type': 'multipart/form-data'
-          }).then((response) => {
-          console.log('SUCCESS!');
-          // Get filename from the response
-          const filename = response.data.filename;
+  <div>
+    <h2>Upload a PDF</h2>
+    <input type="file" @change="onFileChange" accept="application/pdf" />
+    <button @click="upload">Upload</button>
+  </div>
+</template>
 
-          // Set filename as a cookie
-          document.cookie = `filename=${filename}; path=/`;
-        })
-        .catch(() => {
-          console.log('FAILURE!');
-        });
-      }
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      selectedFile: null
+    }
+  },
+  methods: {
+    onFileChange(e) {
+      this.selectedFile = e.target.files[0];
+    },
+    upload() {
+      let formData = new FormData();
+      formData.append('file', this.selectedFile);
+
+      axios.post('https://api.valterbonez.com:8081/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then((response) => {
+        console.log('SUCCESS!');
+        // Get filename from the response
+        const filename = response.data.filename;
+
+        // Set filename as a cookie
+        document.cookie = `filename=${filename}; path=/`;
+      })
+      .catch(() => {
+        console.log('FAILURE!');
+      });
     }
   }
-  </script>
+}
+</script>
